d1df70bf8a23e3c27c219da9f8e15e310d430e7a
Jérémy Buget <jbuget@octo.com>
2016-12-11 23:53:58 +0100

    Add unit tests and configure logger level by environment

--- test/infrastructure/feeders/github-feeder.spec.js
+++ test/infrastructure/feeders/github-feeder.spec.js
@@ -2,6 +2,8 @@ const chai = require('chai');
 const expect = chai.expect;
 const GithubFeeder = require('../../../lib/infrastructure/feeders/github-feeder');
 const Service = require('../../../lib/domain/service');
+const GithubClient = require('../../../lib/infrastructure/github-client');
+const Sinon = require('sinon');
 
 function buildService(githubId) {
   return new Service({
@@ -14,20 +16,65 @@ function buildService(githubId) {
 }
 
 describe('Unit | Feeder | GithubFeeder', function () {
-  describe('#fetchData()', function () {
-    it('should add Github data to Service objects', function () {
+  describe('#feedData()', function () {
+
+    let services;
+    let stub;
+
+    before(function () {
+      services = [];
+      services.push(buildService('user1/repoA'));
+      services.push(buildService('user2/repoB'));
+      services.push(buildService('user3/repoC'));
+    });
+
+    afterEach(function () {
+      GithubClient.getRepo.restore();
+    });
+
+    it('should add Github data to Service objects', function (done) {
       //given
-      const services = [];
-      const service1 = buildService('user1/repoA');
-      const service2 = buildService('user2/repoB');
-      const service3 = buildService('user3/repoC');
-      services.push(service1, service2, service3);
+      stub = Sinon.stub(GithubClient, 'getRepo', () => {
+        return { getDetails(cb) {
+          return new Promise((resolve) => {
+            let result = {};
+            result.id = Math.random() * (10000 - 1000) + 1000;
+            result.name = 'toto';
+            cb(null, result);
+            resolve();
+          });
+        }};
+      });
 
       //when
-      GithubFeeder.fetchData(services);
+      GithubFeeder.feedData(services).then((services) => {
 
-      //then
-      expect(service1.github).to.exist;
+        //then
+        services.forEach((service) => {
+          expect(service.github.id).to.exist;
+          expect(service.github.name).to.equal('toto');
+        });
+        done();
+      });
     });
+
+    it('should reject with error if thrown by GitHub client', function (done) {
+      // given
+      stub = Sinon.stub(GithubClient, 'getRepo', () => {
+        return { getDetails() {
+          return new Promise((resolve, reject) => {
+            reject(new Error('expected error'));
+          })
+        }};
+      });
+
+      //when
+      GithubFeeder.feedData(services).catch(err => {
+
+        // then
+        expect(err.message).to.equal('expected error');
+        done();
+      })
+    })
   });
 });
