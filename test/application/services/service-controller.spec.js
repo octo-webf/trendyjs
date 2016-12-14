d814f3f8a8747e17ae139816cd4c90aea0aeac09
Jérémy Buget <jbuget@gmail.com>
2016-12-15 00:47:29 +0100

    Refector some tests

--- test/application/services/service-controller.spec.js
+++ test/application/services/service-controller.spec.js
@@ -1,5 +1,4 @@
 const chai = require('chai');
-const expect = chai.expect;
 const server = require('../../../server');
 const sinon = require('sinon');
 const ServiceController = require('../../../lib/application/services/service-controller');
@@ -7,30 +6,31 @@ const Cache = require('../../../lib/infrastructure/cache');
 
 describe('Unit | Controller | ServiceController', function () {
   describe('#find', function () {
+    describe('when the request has been cached', function () {
+      let stub;
 
-    let stub;
+      beforeEach(function () {
+        stub = sinon.stub(Cache, 'get');
+      });
 
-    beforeEach(function () {
-      stub = sinon.stub(Cache, 'get');
+      afterEach(function () {
+        Cache.get.restore();
+      });
+
+      it('should return cached response', function () {
+        // given
+        const request = {};
+        const reply = sinon.stub();
+        const response = { services: ['service 1', 'service 2', 'service 3'] };
+        stub.returns(response);
+
+        // when
+        ServiceController.find(request, reply);
+
+        // then
+        sinon.assert.calledWith(reply, response);
+      });
     });
 
-    afterEach(function () {
-      stub.restore();
-    });
-
-    it('should return cached response if it exists without making other treatments', function () {
-      // given
-      const request = {};
-      const reply = sinon.stub();
-      const response = { services: ['service 1', 'service 2', 'service 3'] };
-      stub.returns(response);
-
-      // when
-      ServiceController.find(request, reply);
-
-      // then
-      sinon.assert.calledOnce(stub);
-      sinon.assert.calledWith(reply, response);
-    });
   });
 });
